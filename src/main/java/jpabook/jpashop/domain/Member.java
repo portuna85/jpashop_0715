70e4a53f1659a302243ef0a1bff2e7fcb651e2ba
portuna85 <portuna85@gmail.com>
2022-07-10 09:33:58 +0900

    jpashop

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,11 +1,12 @@
 package jpabook.jpashop.domain;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,6 +15,14 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
 }
